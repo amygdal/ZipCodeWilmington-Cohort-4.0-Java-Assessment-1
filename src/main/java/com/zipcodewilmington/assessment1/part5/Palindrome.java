39aad10e8e6de26e08bac5d0626ea7ea92957cf7
Amy Gill <amygill@MacBook-Pro-2.local>
2018-02-23 11:52:22 -0500

    all tests passing

--- src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
+++ src/main/java/com/zipcodewilmington/assessment1/part5/Palindrome.java
@@ -4,7 +4,19 @@ public class Palindrome {
 
     public Integer countPalindromes(String input){
 
+        Integer numOfPalindromes = 0;
 
-        return null;
+        for (int i = 0; i <= input.length(); i++){
+            for (int j = i+1; j <= input.length(); j++){
+                StringBuilder reverseMyInput = new StringBuilder(input.substring(i,j));
+                reverseMyInput.reverse();
+                if (input.substring(i,j).equals(reverseMyInput.toString())){
+                    numOfPalindromes++;
+                }
+            }
+        }
+
+
+        return numOfPalindromes;
     }
 }
